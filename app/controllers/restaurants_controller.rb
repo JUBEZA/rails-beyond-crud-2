81ed5d69fbeb23cd225b3c0aa9b2ef938d797bbe
Juan Bezanilla <juanbezanillaz@gmail.com>
2024-02-08 17:05:40 -0300

    next is nessted resources

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: %i[ show edit update destroy ]
+  before_action :set_restaurant, only: %i[ show edit update destroy chef]
 
   # GET /restaurants
   def index
@@ -10,9 +10,17 @@ class RestaurantsController < ApplicationController
   def show
   end
 
+  def chef
+    @chef_name = @restaurant.chef_name
+  end
+
   # GET /restaurants/new
   def new
     @restaurant = Restaurant.new
+  end
+
+  def top
+    @restaurants = Restaurant.where(rating:5)
   end
 
   # GET /restaurants/1/edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  resources :restaurants
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+  # Define your application  routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+      collection do
+        get :top
+      end
+
+      member do
+        get :chef
+      end
+    end
 end
