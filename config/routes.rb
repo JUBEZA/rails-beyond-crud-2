d1e05131de00fe064616ff5d3a3c95e3d1f25e21
Juan Bezanilla <juanbezanillaz@gmail.com>
2024-02-14 14:31:46 -0300

    route created

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants do
-  # Define your application  routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-      collection do
-        get :top
-      end
-
-      member do
-        get :chef
-      end
+    resources :reviews, only: [:new]
+ 
     end
 end
